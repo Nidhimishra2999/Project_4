3ba446d23993471fc84c0a6cc732028a8dd80f91
nidhimishra <mnidhi2999@gmail.com>
2023-06-24 15:33:04 +0530

    ORS

---
+++ Project_4/src/main/java/in/co/pro4/exception/RecordNotFoundException.java
@@ -0,0 +1,13 @@
+package in.co.pro4.exception;
+
+/**
+ * RecordNotFoundException thrown when a record not found occurred.
+ * @author Nidhi Mishra 
+ * 
+ */
+public class RecordNotFoundException extends Exception {
+
+	public RecordNotFoundException(String msg) {
+		super(msg);
+	}
+}
